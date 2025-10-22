132f2e5ff3c2f0a60da8ed380bb2949895931caa
Elias Arango Salvador <eliasjuan.as.14@gmail.com>
2025-10-22 01:37:59 -0500

    Commit 2
    Enlazado correctamente a xamp.

--- assets/admin/db.php
+++ assets/admin/db.php
@@ -1,10 +1,12 @@
 <?php
-$host = "localhost:3307";
+$host = "localhost";
+$port = 3306; // Puerto por defecto de MySQL
 $usuario = "root";
-$password = ""; // SIN contraseña
-$basedatos = "tinkuy_db";
+$password = ""; // Tu contraseña (está bien si es vacía en XAMPP)
+$database = "tinkuy_db";
 
-$conn = new mysqli($host, $usuario, $password, $basedatos);
+// Pasamos el puerto $port como el quinto parámetro
+$conn = new mysqli($host, $usuario, $password, $database);
 
 if ($conn->connect_error) {
     die("Error de conexión: " . $conn->connect_error);
