a05d2081b9caae0cfd5a45393d8bc2abdd78c0c9
Elias Arango Salvador <eliasjuan.as.14@gmail.com>
2025-11-06 02:00:03 -0500

    Update footeer

--- src/Views/components/footer.php
+++ src/Views/components/footer.php
@@ -33,14 +33,6 @@
 })(document);
 </script>
 
-<script>
-(function(d){
-  var s = d.createElement("script");
-  s.setAttribute("data-account", "OnvWxZDKAe");
-  s.setAttribute("src", "https://cdn.userway.org/widget.js");
-  (d.body || d.head).appendChild(s);
-})(document);
-</script>
 
 
 <style>
